e2cd906d02fd08b33647fa9b3b0acc4d6c1763ad
gayanthusith <officialdileepgayan@gmail.com>
2023-04-23 22:45:53 +0530

    Add files via upload

---
+++ docs/js/script.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal ({
+    distance: '85px',
+    duration: 3000,
+    delay: 450,
+    reset: true
+    });
+
+    sr. reveal('.home-info', {delay:400, origin: 'top'});
+    // sr. reveal('.home-img img' , {delay:400, origin: 'bottom'});
